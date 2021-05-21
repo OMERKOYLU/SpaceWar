1b9b284f9e7714a6f05250967bf8cf6a6c31f733
OMERKOYLU <ufukyasar@windowslive.com>
2021-05-21 17:53:09 +0300

    Zıplama Komutları Eklendi

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ x=400
 y=300
 ziplamaGucu=30
 yukseklik=y
+havada=False
 while devamet:
     pygame.time.Clock().tick(10)
     olaylar = pygame.event.get()
@@ -18,22 +19,24 @@ while devamet:
         if olay.type == pygame.QUIT:
             devamet = False
     ekran.fill((255, 255, 255))
-
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
         player.newYon=1
         x+=5
         player.updateKukla(ekran, x, y, "kos")
-    if pygame.key.get_pressed()[pygame.K_LEFT]:
+    elif pygame.key.get_pressed()[pygame.K_LEFT]:
         player.newYon=-1
         x-=5
         player.updateKukla(ekran, x, y, "kos")
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
-        ziplamaGucu-=5
-        y-=5
-        if yukseklik<270:
-            ziplamaGucu=0
-            y+=5
+    elif pygame.key.get_pressed()[pygame.K_SPACE]:
+        if not havada:
+            havada=True
+            ziplamaGucu = 5
+    elif havada:
+        y -= ziplamaGucu
+        ziplamaGucu -= 1
+        if (ziplamaGucu == -5):
+            havada = False
         player.updateKukla(ekran, x, y, "zipla")
-    if not (True in pygame.key.get_pressed()):
+    else:
         player.updateKukla(ekran, x, y, "dur")
     pygame.display.update()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ufuk/PycharmProjects/pygameSprite/venv"
+set -gx VIRTUAL_ENV "/home/ufuk/Documents/atolye/SpaceWar/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
